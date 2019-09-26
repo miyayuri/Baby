886ee84c89ddee99d0f437a9f24bd8c3229602be
miyayuri <yurikomiyamoto819@gmail.com>
2019-09-26 09:43:45 +0000

    コンフリクト解消

--- config/puma.rb
+++ config/puma.rb
@@ -33,8 +33,4 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-<<<<<<< Updated upstream
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-=======
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
->>>>>>> Stashed changes
